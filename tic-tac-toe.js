ffb8ff6baca456cdbf9da5f13ce43ed8ed51abab
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-05-01 15:07:12 -0700

    Add board moves array, update naming

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,17 @@
 var expect = require('expect');
 var readline = require('readline');
+// Pulled from https://nodejs.org/api/readline.html
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 var TicTacToe = function() {
-	this.playerOne = '',
-	this.playerTwo = '',
-	this.moves = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+  this.playerOne = '',
+  this.playerTwo = '',
+  this.currentBoard = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+  this.boardMove = ['top left', 'top', 'top right', 'middle left', 'middle', 'middle right', 'bottom left', 'bottom', 'bottom right'];
+  this.totalMoves = 0;
 };
 
 TicTacToe.prototype.printGameBoard = function() {
@@ -15,21 +22,22 @@ TicTacToe.prototype.printGameBoard = function() {
   console.log(this.moves.slice(6, 9).join(' | '));
 };
 
-// Pulled from https://nodejs.org/api/readline.html
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+TicTacToe.prototype.playGame = function() {
+  rl.question('Whose player One? ', function(answer) {
+    this.playerOne = answer;
+    rl.question('Whose player Two? ', function(answer) {
+      this.playerTwo = answer;
+      this.printGameBoard();
+      this.
 
-rl.question('Whose player One? ', function(answer) {
+      rl.on('line', function (input) {
 
-	rl.question('Whose player Two? ', function(answer) {
-		gameBoard([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']);
-	});
-});
+      });
 
+    });
+  });
+};
 
-rl.on('line', function (input) {
-  console.log();
-});
+TicTacToe.prototype.moves = function() {
 
+};
